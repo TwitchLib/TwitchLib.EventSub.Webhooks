367d4dd647c429f88a709de45e99c10470626db8
AoshiW <ondru@LAPTOP-7K2AEST8>
2025-07-14 21:07:52 +0200

    add handling for unknow events

---
+++ TwitchLib.EventSub.Webhooks/Core/EventArgs/UnknownEventSubEventArgs.cs
@@ -0,0 +1,7 @@
+﻿using System.Text.Json;
+using TwitchLib.EventSub.Webhooks.Core.Models;
+
+namespace TwitchLib.EventSub.Webhooks.Core.EventArgs;
+
+public class UnknownEventSubEventArgs : TwitchLibEventSubEventArgs<EventSubNotificationPayload<JsonElement>>
+{ }
